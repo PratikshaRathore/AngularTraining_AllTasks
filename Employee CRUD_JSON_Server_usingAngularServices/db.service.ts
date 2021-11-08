d882815d4a1b233f8a9dc685d89592c9aff15896
PratikshaRathore <93536616+PratikshaRathore@users.noreply.github.com>
2021-11-08 15:36:50 +0530

    Add files via upload

---
+++ Employee CRUD_JSON_Server_usingAngularServices/db.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbService {
+
+  dataArray:any[] =  [];
+  url:string  = "http://localhost:3000/emps";
+
+  constructor(private httpObj: HttpClient) {
+
+  }
+
+  getData():Observable<any[]>
+  {
+      return this.httpObj.get<any[]>(this.url);
+  }
+
+  getDataById(eno:number) : Observable<any>
+  {
+   // return this.httpObj.get<any>(this.url + "/" + dno);
+    return this.httpObj.get<any>(`${this.url}/${eno}`);
+  }
+
+  addData(dataObj:any) : Observable<any>
+  {
+    return this.httpObj.post<any>(this.url, dataObj);
+  }
+
+  updateData(dataObj:any) : Observable<any>
+  {
+    return this.httpObj.put<any>(this.url + "/" + dataObj.empno, dataObj);
+  }
+
+  deleteData(eno:number) : Observable<any>
+  {
+    return this.httpObj.delete<any>(`${this.url}/${eno}`);
+  }
+}
